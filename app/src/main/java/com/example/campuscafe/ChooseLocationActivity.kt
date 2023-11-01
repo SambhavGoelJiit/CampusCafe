491a0610c138bbe46056727deef48ebae7022bf2
Sambhav Goel <possible.goel@gmail.com>
2023-11-01 23:43:44 +0530

    User App Complete

--- app/src/main/java/com/example/campuscafe/ChooseLocationActivity.kt
+++ app/src/main/java/com/example/campuscafe/ChooseLocationActivity.kt
@@ -1,10 +1,13 @@
 package com.example.campuscafe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.ArrayAdapter
 import com.example.campuscafe.databinding.ActivityChooseLocationBinding
 
+@Suppress("DEPRECATION")
 class ChooseLocationActivity : AppCompatActivity() {
     private val binding: ActivityChooseLocationBinding by lazy{
         ActivityChooseLocationBinding.inflate(layoutInflater)
@@ -16,5 +19,10 @@ class ChooseLocationActivity : AppCompatActivity() {
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, locationList)
         val autoCompleteTextView = binding.listOfLocation
         autoCompleteTextView.setAdapter(adapter)
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
--- app/src/main/java/com/example/campuscafe/SignActivity.kt
+++ app/src/main/java/com/example/campuscafe/SignActivity.kt
@@ -1,11 +1,22 @@
 package com.example.campuscafe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.campuscafe.databinding.ActivityLoginBinding
+import com.example.campuscafe.databinding.ActivitySignBinding
 
 class SignActivity : AppCompatActivity() {
+
+    private val binding: ActivitySignBinding by lazy{
+        ActivitySignBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign)
+        setContentView(binding.root)
+        binding.createAccButton.setOnClickListener{
+            val intent = Intent(this, ChooseLocationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
