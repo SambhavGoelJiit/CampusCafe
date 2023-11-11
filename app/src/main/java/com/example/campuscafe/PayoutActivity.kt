476661408c74f2165a93716758c32343b4494814
Sambhav Goel <possible.goel@gmail.com>
2023-11-11 15:08:27 +0530

    User App Cart + Profile

--- app/src/main/java/com/example/campuscafe/PayoutActivity.kt
+++ app/src/main/java/com/example/campuscafe/PayoutActivity.kt
@@ -2,17 +2,63 @@ package com.example.campuscafe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import com.example.campuscafe.databinding.ActivityPayoutBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.StorageTask.SnapshotBase
 
 class PayoutActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityPayoutBinding
+    lateinit var binding: ActivityPayoutBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var databaseReference: DatabaseReference
+    private lateinit var totalAmount: String
+    private lateinit var userId: String
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+        databaseReference = FirebaseDatabase.getInstance().getReference()
+        setUserData()
+
         binding.placeMyOrder.setOnClickListener {
             val bottomSheetDialog = CongratsBottomSheet()
             bottomSheetDialog.show(supportFragmentManager, "Test")
+        }
+    }
+
+    private fun setUserData() {
+        val user = auth.currentUser
+        if (user != null) {
+            val userId = user.uid
+            val userReference = databaseReference.child("userMainApp").child(userId)
+
+            userReference.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
+                        val names = snapshot.child("usernameModel").getValue(String::class.java) ?: ""
+                        val phones = snapshot.child("phoneModel").getValue(String::class.java)?:""
+
+                        binding.apply {
+                            custName.setText(names)
+                            custPhone.setText(phones)
+                        }
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+            })
         }
     }
 }
--- app/src/main/java/com/example/campuscafe/model/UserModel.kt
+++ app/src/main/java/com/example/campuscafe/model/UserModel.kt
@@ -3,6 +3,7 @@ package com.example.campuscafe.model
 data class UserModel(
     val usernameModel: String? = null,
     val emailModel: String? = null,
-    val passwordModel: String? = null
+    val passwordModel: String? = null,
+    val phoneModel: String? = null
 )
 
